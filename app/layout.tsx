09eb54a65c431527fe3b7edfd2450b4785c8989d
Patryk Podworski <patryk.podworski@gmail.com>
2023-12-29 11:28:43 +0100

    Add Google Analytics script (#1)
    Co-authored-by: Patryk Podworski <patryk@xtramile.no>

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import GoogleTag from "./GoogleTag";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={clsx(inter.className, "bg-neutral-900")}>
         {children}
       </body>
+      <GoogleTag />
     </html>
   );
 }
