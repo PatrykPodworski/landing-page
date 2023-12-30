30d17db32377aa3d59b2aa2d19b65569612d0e54
Patryk Podworski <patryk@xtramile.no>
2023-12-31 00:44:06 +0100

    test week component

--- components/Month/Week.test.tsx
+++ components/Month/Week.test.tsx
@@ -1,7 +1,51 @@
 import { render, screen } from "@testing-library/react";
 import Week from "./Week";
 
-test("always renders 7 days", () => {
-  render(<Week />);
-  expect(screen.getAllByRole("button").length).toBe(7);
+describe("always renders 7 days", () => {
+  it.each([
+    [0, 0, 0],
+    [6, 6, 6],
+    [0, 6, 3],
+    [1, 6, 1],
+    [0, 3, 0],
+  ])(
+    "when startDay is %i and endDay is %i, activeDay is %i",
+    (startDay, endDay, activeDay) => {
+      render(
+        <Week startDay={startDay} endDay={endDay} activeDay={activeDay} />
+      );
+      expect(screen.getAllByRole("button").length).toBe(7);
+    }
+  );
+});
+
+describe("throws error when startDay is greater than endDay", () => {
+  it.each([
+    [1, 0],
+    [2, 1],
+    [3, 2],
+    [4, 3],
+    [5, 4],
+    [6, 5],
+  ])("when startDay is %i and endDay is %i", (startDay, endDay) => {
+    expect(() => {
+      render(<Week startDay={startDay} endDay={endDay} />);
+    }).toThrow();
+  });
+});
+
+describe("throws error when activeDay is not between startDay and endDay", () => {
+  it.each([
+    [0, 5, 6],
+    [1, 6, 0],
+  ])(
+    "when startDay is %i, endDay is %i, and activeDay is %i",
+    (startDay, endDay, activeDay) => {
+      expect(() => {
+        render(
+          <Week startDay={startDay} endDay={endDay} activeDay={activeDay} />
+        );
+      }).toThrow();
+    }
+  );
 });
