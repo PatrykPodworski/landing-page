f03cc8278e43ce132a74f77de1ff7cc820b38e76
Patryk Podworski <patryk.podworski@gmail.com>
2024-01-23 00:56:28 +0100

    Get current month and year and use it in get habits query

--- lib/db/HabitItem/queryByUserId/getCommand.tsx
+++ lib/db/HabitItem/queryByUserId/getCommand.tsx
@@ -1,15 +1,32 @@
 import getEnv from "@/utils/getEnv";
 import { QueryCommand } from "@aws-sdk/client-dynamodb";
 
-const getCommand = (userId: string) => {
+// TODO: Add index for Month and Year
+const getCommand = (
+  userId: string,
+  currentMonth: number,
+  currentYear: number
+) => {
   const tableName = getEnv("TABLE_NAME");
 
   return new QueryCommand({
     TableName: tableName,
-    KeyConditionExpression: "UserId = :UserId",
+    KeyConditionExpression: "#UserId = :UserId",
+    FilterExpression: "#Month = :Month AND #Year = :Year",
+    ExpressionAttributeNames: {
+      "#UserId": "UserId",
+      "#Month": "Month",
+      "#Year": "Year",
+    },
     ExpressionAttributeValues: {
       ":UserId": {
         S: userId,
+      },
+      ":Month": {
+        N: (currentMonth + 1).toString(),
+      },
+      ":Year": {
+        N: currentYear.toString(),
       },
     },
   });
--- lib/habits/getHabits/index.tsx
+++ lib/habits/getHabits/index.tsx
@@ -3,11 +3,15 @@ import mapToCompletedItem from "./mapToCompletedItem";
 import groupItems from "./groupItems";
 import { queryByUserId } from "@/lib/db/HabitItem";
 
-const getHabits = async (inputUserId: string | undefined) => {
+const getHabits = async (
+  inputUserId: string | undefined,
+  currentMonth: number,
+  currentYear: number
+) => {
   const userId = getEnv("USER_ID");
   const showRealData = userId === inputUserId;
 
-  const itemsFromDb = await queryByUserId(userId);
+  const itemsFromDb = await queryByUserId(userId, currentMonth, currentYear);
   const completedItems = itemsFromDb.map(mapToCompletedItem);
   const habits = groupItems(completedItems, showRealData);
 
