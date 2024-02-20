60bc9df1a272d8989dc5029295ff0aae219c4812
Patryk Podworski <patryk.podworski@gmail.com>
2024-02-21 00:38:01 +0100

    When an item exists, update source instead of skipping it during sync (#19)

--- lib/db/HabitItem/index.ts
+++ lib/db/HabitItem/index.ts
@@ -1,6 +1,8 @@
 import { HabitItemDbo } from "./HabitItemDbo";
 import insertIfNotExists from "./insertIfNotExists/insertIfNotExists";
 import queryByUserId from "./queryByUserId";
+import querySingle from "./querySingle";
+import putItem from "./putItem";
 
-export { insertIfNotExists, queryByUserId };
+export { insertIfNotExists, queryByUserId, querySingle, putItem };
 export type { HabitItemDbo };
--- lib/habits/synchronizeHabits/insertItems.tsx
+++ lib/habits/synchronizeHabits/insertItems.tsx
@@ -1,8 +1,28 @@
-import { HabitItemDbo, insertIfNotExists } from "@/lib/db/HabitItem";
+import { HabitItemDbo, putItem, querySingle } from "@/lib/db/HabitItem";
 
 const insertItems = async (items: HabitItemDbo[]) => {
   for (const item of items) {
-    await insertIfNotExists(item);
+    await insertItem(item);
+  }
+};
+
+const insertItem = async (item: HabitItemDbo) => {
+  const queriedItem = await querySingle({
+    habitId: item.HabitId,
+    userId: item.UserId,
+    day: item.Day,
+    month: item.Month,
+    year: item.Year,
+  });
+
+  if (!queriedItem) {
+    await putItem(item);
+    return;
+  }
+
+  if (queriedItem.Source === "{}") {
+    const updatedItem = { ...queriedItem, Source: item.Source };
+    await putItem(updatedItem);
   }
 };
 
