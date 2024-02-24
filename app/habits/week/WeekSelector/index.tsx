6807aca2762d226c032516aab410aa2798ddccd6
Patryk Podworski <patryk.podworski@gmail.com>
2024-02-24 13:28:41 +0100

    Support dual year weeks

--- app/habits/week/WeekSelector/index.tsx
+++ app/habits/week/WeekSelector/index.tsx
@@ -31,15 +31,14 @@ export type WeekYear = {
   year: number;
 };
 
-// TODO: Weekly Page: Support dual year weeks
-// TODO: Weekly Page: Loop the weeks
+// TODO: Weekly Page: Show month days
+// TODO: Weekly Page: Week change animation
+// TODO: Weekly Page: Today button
 const WeekLabel = ({ weekYear: { week, year } }: WeekLabelProps) => {
-  const monthLabel = getWeekDates(week, year);
+  const label = getLabel(week, year);
   return (
     <div className="flex gap-3 items-baseline ">
-      <h1 className="text-neutral-100 text-lg">
-        {monthLabel} {year}
-      </h1>
+      <h1 className="text-neutral-100 text-lg">{label}</h1>
       <small className="rounded px-2 bg-neutral-300 font-medium text-neutral-950 text-sm">
         Week {week}
       </small>
@@ -47,16 +46,23 @@ const WeekLabel = ({ weekYear: { week, year } }: WeekLabelProps) => {
   );
 };
 
-const getWeekDates = (week: number, year: number) => {
+const getLabel = (week: number, year: number) => {
   const date = new Date(year, 0, 1 + (week - 1) * 7);
   const weekStart = startOfWeek(date, { weekStartsOn: 1 });
   const weekEnd = endOfWeek(date, { weekStartsOn: 1 });
   const isSameMonth = weekStart.getMonth() === weekEnd.getMonth();
+  const isSameYear = weekStart.getFullYear() === weekEnd.getFullYear();
   if (isSameMonth) {
-    return format(weekStart, "MMMM");
+    return `${format(weekStart, "MMMM")} ${format(weekStart, "yyyy")}`;
+  }
+  if (isSameYear) {
+    return `${format(weekStart, "MMM")} - ${format(weekEnd, "MMM")} ${format(
+      weekStart,
+      "yyyy"
+    )}`;
   }
 
-  return `${format(weekStart, "MMM")} - ${format(weekEnd, "MMM")}`;
+  return `${format(weekStart, "MMM yyyy")} - ${format(weekEnd, "MMM yyyy")}`;
 };
 
 type WeekLabelProps = {
