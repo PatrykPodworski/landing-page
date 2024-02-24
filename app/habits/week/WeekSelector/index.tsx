38d80e93017c8f2c655148124005151d124a36bc
Patryk Podworski <patryk.podworski@gmail.com>
2024-02-24 13:18:11 +0100

    Dynamic week selector month label

--- app/habits/week/WeekSelector/index.tsx
+++ app/habits/week/WeekSelector/index.tsx
@@ -1,3 +1,4 @@
+import { endOfWeek, format, startOfWeek } from "date-fns";
 import { IconButton } from "@/components";
 import { ChevronLeft, ChevronRight } from "@/icons";
 
@@ -7,7 +8,7 @@ const WeekSelector = ({
   onPreviousWeek,
 }: WeekSelectorProps) => {
   return (
-    <div className="flex w-full justify-between mb-6">
+    <div className="flex w-full justify-between items-center mb-4">
       <IconButton onClick={onPreviousWeek}>
         <ChevronLeft />
       </IconButton>
@@ -30,16 +31,32 @@ export type WeekYear = {
   year: number;
 };
 
-// TODO: Weekly Page: Support dual month weeks
+// TODO: Weekly Page: Support dual year weeks
+// TODO: Weekly Page: Loop the weeks
 const WeekLabel = ({ weekYear: { week, year } }: WeekLabelProps) => {
+  const monthLabel = getWeekDates(week, year);
   return (
     <div className="flex gap-3 items-baseline ">
-      <h1 className="text-neutral-100 text-lg">February {year}</h1>
+      <h1 className="text-neutral-100 text-lg">
+        {monthLabel} {year}
+      </h1>
       <small className="rounded px-2 bg-neutral-300 font-medium text-neutral-950 text-sm">
         Week {week}
       </small>
     </div>
   );
+};
+
+const getWeekDates = (week: number, year: number) => {
+  const date = new Date(year, 0, 1 + (week - 1) * 7);
+  const weekStart = startOfWeek(date, { weekStartsOn: 1 });
+  const weekEnd = endOfWeek(date, { weekStartsOn: 1 });
+  const isSameMonth = weekStart.getMonth() === weekEnd.getMonth();
+  if (isSameMonth) {
+    return format(weekStart, "MMMM");
+  }
+
+  return `${format(weekStart, "MMM")} - ${format(weekEnd, "MMM")}`;
 };
 
 type WeekLabelProps = {
