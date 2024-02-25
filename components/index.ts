499a6a8002d8b406b962cd0d1e769eb870698d7c
Patryk Podworski <patryk.podworski@gmail.com>
2024-02-25 18:34:35 +0100

    Move WeekSelector to components

--- components/index.ts
+++ components/index.ts
@@ -1,10 +1,15 @@
 import Button from "./Button";
 import IconButton from "./IconButton";
-export { Button, IconButton };
 import Day from "./Month/Day";
 import Week from "./Month/Week";
 import Month from "./Month/Month";
 import DayStatus from "./Month/DayStatus";
 import MonthSkeleton from "./Month/MonthSkeleton";
+import WeekSelector from "./WeekSelector";
+
+export { Button, IconButton };
+
 export { Day, Week, Month, MonthSkeleton };
 export type { DayStatus };
+
+export { WeekSelector };
