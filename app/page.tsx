efff0f838c0bb7926d2ac71d7384ed38488f3df4
Patryk Podworski <patryk.podworski@gmail.com>
2024-11-16 10:23:55 +0100

    Simple animated logo and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import { LogoAnimated } from "@/components/Logo/LogoAnimated";
+
+// TODO: Update packages
 const Home = () => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-8 items-center">
+      <LogoAnimated />
       <h1 className="text-slate-50 text-5xl mb-4">Hello, I&apos;m Patryk</h1>
-      <h2 className="text-slate-300 text-2xl mb-2">
-        This is a work in progress
-      </h2>
     </div>
   );
 };
---
+++ components/Logo/LogoAnimated.tsx
@@ -0,0 +1,7 @@
+"use client";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
+// TODO: Suspense with svg
+export const LogoAnimated = () => (
+  <DotLottieReact src="/logo-animated.lottie" autoplay loop />
+);
