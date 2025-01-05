068d59fc4583eb08886f1c830209cd746c611343
Patryk Podworski <patryk.podworski@gmail.com>
2025-01-05 18:32:48 +0100

    disable selecting of text (#24)

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="flex flex-col gap-8 items-center">
       <LogoAnimated />
-      <h1 className="text-slate-50 text-4xl mb-4 text-center">
+      <h1 className="text-slate-50 text-4xl mb-4 text-center select-none">
         Hello, I&apos;m Patryk
       </h1>
     </div>
